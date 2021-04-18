21adf100e67f56d58c2312c622b6c3d2c9b050e2
Rahul Chocha <49686817+mrchocha@users.noreply.github.com>
2021-04-18 14:49:26 +0530

    Update settings.py

--- colorAPI/settings.py
+++ colorAPI/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*@nr&ob4_dry=h#*tzq7ufaj_2xy3(=0ak7$+ybmf-fa0*c6&p'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
